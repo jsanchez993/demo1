ef11e08fabcb4b1cd28c22180a03805f08043f36
jsanchez993 <jonathansanchez993@gmail.com>
2024-10-24 15:29:34 -0400

    fix-2

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 import { LogOut, FileText } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
